69e3dac65306a4497e4e1b161ecf977a5c8aa8b9
harinder24 <hss022002@gmail.com>
2023-04-05 16:49:40 -0700

    ui all done

--- src/Components/Container/MiddleContainer.jsx
+++ src/Components/Container/MiddleContainer.jsx
@@ -10,13 +10,17 @@ import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
 import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import CloseIcon from '@mui/icons-material/Close';
 import { useEffect } from "react";
 export default function MiddleContainer() {
-  useEffect(() => {
-    document.getElementById("postBtn").addEventListener("click", function(){
-      console.log("yes");
-    })
-  }, []);
+  function showCreateAPost(){
+    document.getElementById("FeedModalID").classList.toggle("ShowDisplayFlex")
+    document.querySelector("html").classList.toggle("htmlOverflow")
+  }
+  function closeCreateAPost(){
+    document.getElementById("FeedModalID").classList.remove("ShowDisplayFlex")
+    document.querySelector("html").classList.remove("htmlOverflow")
+  }
   return (
     <div className="MainMiddleContainer">
       <div className="MiddleContainer">
@@ -26,7 +30,7 @@ export default function MiddleContainer() {
             alt=""
           />
          
-          <button id="postBtn">Start a post</button>
+          <button onClick={showCreateAPost} id="postBtn">Start a post</button>
         </div>
         <div className="MiddleContainerIcon">
           <MiddleContainerStartAPost
@@ -52,6 +56,33 @@ export default function MiddleContainer() {
         </div>
       </div>
       <UserFeed name="Harinderpal Sran" description="Description" message="Wow this works"/>
+      <div id="FeedModalID" className="FeedModal">
+          <div className="AddFeed">
+              <div className="AddFeed_header">
+                <h4>Create a post</h4>
+                <div onClick={closeCreateAPost}>
+                <CloseIcon />
+                </div>
+                
+              </div>
+              <div className="AddFeed_user">
+              <Avatar className="MiddleContainer_Aatar"
+            src="https://media.licdn.com/dms/image/D5603AQHnp7fhICaZ9Q/profile-displayphoto-shrink_100_100/0/1674352141393?e=1685577600&v=beta&t=0WAaQ7sL3slXJqW8mCdezxuqTkVnynoA-8eDR_2e1QA"
+            alt=""
+          />
+
+          <div>Harinderpal Sran</div>
+              </div>
+
+              <div className="AddFeed_input">
+                <textarea name="" id="" placeholder="What do you want to talk about?" autoFocus></textarea>
+              </div>
+              <div className="AddFeed_btn">
+              <button>Post</button>
+              </div>
+              
+          </div>
+      </div>
     </div>
   );
 }
--- src/Components/Header/IconAndSearch.jsx
+++ src/Components/Header/IconAndSearch.jsx
@@ -3,48 +3,57 @@ import "./IconAndSearch.css"
 import SearchIcon from '@mui/icons-material/Search';
 import { useEffect, useState  } from 'react';
 export default function IconAndSearch() {
-const [truth, setTruth] = useState(true)
-const setTruthHandler = () => {
-  setTruth(!truth);
-}
-  useEffect(() => {
+
+
+
+
+
+   
+  const addToggle = () => {
     const searchContainer = document.getElementById('SearchContainerID');
     
-    const searchContainerIcon = document.getElementById('SearchIcon_icon_id');
+  const searchContainerIcon = document.getElementById('SearchIcon_icon_id');
+  const headerLeftOptions = document.getElementById('header_left_options');
+    // headerLeftOptions.style.display = "none";
+    headerLeftOptions.classList.add('hide');
+    searchContainer.classList.toggle('SearchContainerToggle');
+    searchContainer.lastElementChild.classList.toggle('SecarchInputToggle');
+  }
+  const removeToggle = () => {
+    const searchContainer = document.getElementById('SearchContainerID');
+    
+  const searchContainerIcon = document.getElementById('SearchIcon_icon_id');
+  const headerLeftOptions = document.getElementById('header_left_options');
+    // headerLeftOptions.style.display = "flex";
+    headerLeftOptions.classList.remove('hide');
+    searchContainer.classList.remove('SearchContainerToggle')
+    searchContainer.lastElementChild.classList.remove('SecarchInputToggle');
+  }
+  function SearchIconHandler(){
     const headerLeftOptions = document.getElementById('header_left_options');
-    const handleClick = () => {
-      if (window.innerWidth <= 768) {
-        if(truth){
-        headerLeftOptions.style.display = "none";
-       
-        searchContainer.classList.toggle('SearchContainerToggle');
-        searchContainer.lastElementChild.classList.toggle('SecarchInputToggle');
-        setTruthHandler()
+    if (window.innerWidth <= 768){
+    
+        if(headerLeftOptions.classList.contains("hide")){
+          removeToggle()
+ 
         
         }else{
-          removeToggle()
-          setTruthHandler()
+          addToggle()
+       
         }
-      }
-    };
-    const removeToggle = () => {
-      headerLeftOptions.style.display = "flex";
-  
-      searchContainer.classList.remove('SearchContainerToggle')
-      searchContainer.lastElementChild.classList.remove('SecarchInputToggle');
+      
     }
-    searchContainer.addEventListener('click', handleClick);
-  
-    return () => {
-      searchContainer.removeEventListener('click', handleClick);
-    };
-  }, [truth]);
-  
+    else if(headerLeftOptions.style.display == "none"){
+      removeToggle()
+      
+    }
+  }
+
   return (
     <div className='Search_Icon_Wrap'>
         <img className='IconAndSearch_img' src="https://pngmind.com/wp-content/uploads/2019/08/Linkedin-Logo-Png-Transparent-Background-1.png" alt="linkedln" />
-        <div id='SearchContainerID' className='SearchContainer'>
-            <SearchIcon id="SearchIcon_icon_id" className="SearchIcon_icon" />
+        <div  id='SearchContainerID' className='SearchContainer'>
+            <SearchIcon onClick={SearchIconHandler} id="SearchIcon_icon_id" className="SearchIcon_icon" />
             <input className='SecarchInput' type="text" placeholder='Search'/>
         </div>
     </div>
